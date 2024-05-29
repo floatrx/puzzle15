a99d83b4017fcaadfab391727548612d107927ab
Andrew floatrx <ifloatrx@gmail.com>
2024-05-30 00:35:55 +0300

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
-function App() {
-  return <>App</>;
-}
-
-export default App;
+export const App = () => (
+  <>
+    <header className="header">
+      <div className="container">
+        <h1 className="text-2xl">React + Vite + Tailwind CSS</h1>
+      </div>
+    </header>
+    <main className="main">
+      <div className="container">Main content!</div>
+    </main>
+    <footer className="footer">
+      <div className="container">
+        Made with{' '}
+        <span role="img" aria-label="love">
+          ❤️
+        </span>{' '}
+        <a href="http://localhost:3000">by @floatrx</a>
+      </div>
+    </footer>
+  </>
+);
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import { Config } from 'tailwindcss';
+
+export default {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+      },
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
